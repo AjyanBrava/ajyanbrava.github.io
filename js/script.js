a4a81b4aefc23f2d089ea5d05d66a1743844139e
ajyanbrava <ajbrbi@gmail.com>
2025-02-14 07:06:03 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementsByID("navbar").style.top = "0";
+  } else {
+    document.getElementsByID("navbar").style.top = "-50px";
+  }
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+};
